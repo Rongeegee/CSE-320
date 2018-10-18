3bc0c6b3416aa943705193836a07353113b6f09c
Rongan Li <Rongan.li@stonybrook.edu>
2018-10-18 17:50:04 -0400

    what i have so far

--- hw3/src/sfmm.c
+++ hw3/src/sfmm.c
@@ -8,10 +8,42 @@
 #include <string.h>
 #include "debug.h"
 #include "sfmm.h"
+#include <errno.h>
 
 void *sf_malloc(size_t size) {
+    if(size == 0)
+        return NULL;
+
+    if(sf_mem_start() == sf_mem_end()){
+        void* ptrNewPage = sf_mem_grow();
+        sf_prologue prologue;
+        prologue.header = *((sf_header*)(ptrNewPage + 8));
+        prologue.footer = *((sf_footer*)(ptrNewPage + 24));
+        sf_epilogue epilogue;
+        epilogue.footer = *((sf_footer*)(ptrNewPage - 7));
+    }
+
+    else{
+        sf_header header;
+        header.info.block_size += size;
+        if(header.info.block_size % 16 != 0){
+            header.info.block_size += (16 - (header.info.block_size % 16));
+        }
+        if(header.info.block_size < 32)
+            header.info.block_size += 16;
+
+        //now search the free list to obtain the first block on the first free list.
+        //node is the sentinel of the free list.
+        sf_free_list_node *node = &sf_free_list_head;
+        node = (*node).next; //now, it is the first node of the list
+    }
     return NULL;
+
 }
+
+
+
+
 
 void sf_free(void *pp) {
     return;
