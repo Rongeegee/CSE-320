aae6847d5e4b9f19d811941664114f56b3b45304
Rongan Li <Rongan.li@stonybrook.edu>
2018-10-20 18:28:37 -0400

    block footer and header setup

--- hw3/src/sfmm.c
+++ hw3/src/sfmm.c
@@ -19,11 +19,11 @@ void *sf_malloc(size_t size) {
         return NULL;
 
     if(sf_mem_start() == sf_mem_end()){
-        void* ptrNewPage = sf_mem_grow();
-        memset(ptrNewPage,0,48);
-        memset((sf_mem_end()  - 8),0,8);
+        void* ptrBeginning = sf_mem_grow();
+        memset(ptrBeginning,0,48);
+        memset((sf_mem_end()  - 16),0,16);
 
-        prologue = (sf_prologue*)(ptrNewPage);
+        prologue = (sf_prologue*)(ptrBeginning);
         (*prologue).header.info.allocated = 1;
         (*prologue).footer.info.allocated = 1;
         epilogue = (sf_epilogue*)(sf_mem_end()  - 8);
@@ -37,14 +37,33 @@ void *sf_malloc(size_t size) {
         if(block_size < 32)
             block_size += 16;
 
+        //gotta check if there is enough bytes to allocate
+        size_t remaining_byte = 4096 - 64;
+        while (remaining_byte <= block_size){
+            sf_mem_grow();
+            memset(epilogue,0,8);
+            epilogue = (sf_epilogue*)(sf_mem_end()  - 8);
+            memset(epilogue,0,8);
+            (*epilogue).footer.info.allocated = 1;
+            remaining_byte += 4096;
+        }
+
         //prev_allocated bit hasn't been figured out yet.
         sf_header block_header;
         block_header.info.requested_size = size;
         block_header.info.block_size = (block_size >> 4);
+        block_header.info.prev_allocated = 1;
         block_header.info.allocated = 1;
-        memcpy(ptrNewPage + 40, &block_header,8);
+        memcpy(ptrBeginning + 40, &block_header,8);
 
-        sf_show_heap();
+
+
+        sf_footer block_footer;
+        block_footer.info.requested_size = size;
+        block_footer.info.block_size = (block_size >> 4);
+        block_footer.info.allocated = 1;
+        memcpy(ptrBeginning + 48 + block_size,&block_footer,8);
+
     }
 
     else{
@@ -58,19 +77,19 @@ void *sf_malloc(size_t size) {
 
         //now search the free list to obtain the first block on the first free list.
         //head is the sentinel of the free list.
-    //     sf_free_list_node *head = &sf_free_list_head;
-    //     while (getBlockOfFitSize(head,header) == NULL){
-    //         if(sf_mem_grow() == NULL){
-    //             sf_errno = ENOMEM;
-    //             return NULL;
-    //         }
-    //         else{
+        // sf_free_list_node *head = &sf_free_list_head;
+        // while (getBlockOfFitSize(head,header) == NULL){
+        //     void* ptrBeginning = sf_mem_grow();
+        //     if(sf_mem_grow() == NULL){
+        //         sf_errno = ENOMEM;
+        //         return NULL;
+        //     }
+        //     else{
 
-    //         }
+        //     }
 
-    //     }
+        // }
     }
-
     return NULL;
 }
 
@@ -82,7 +101,7 @@ sf_free_list_node * getBlockOfFitSize(sf_free_list_node * head, sf_header header
         //now, head is the first node of the list
         sf_free_list_node* current_node = (*head).next;
         while(current_node != &sf_free_list_head){
-            if(current_node->size >= header.info.block_size){
+            if(((current_node->size) << 4) >= header.info.block_size){
                     return current_node;
                 }
             current_node = (*current_node).next;
