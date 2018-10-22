b8f459ce429319db2608d2f7dc28f0b63f1af374
Rongan Li <Rongan.li@stonybrook.edu>
2018-10-22 16:25:17 -0400

    first if statement done in sf_malloc(), but has not been debugged.

--- hw3/src/sfmm.c
+++ hw3/src/sfmm.c
@@ -11,6 +11,7 @@
 #include <errno.h>
 
 sf_free_list_node * getBlockOfFitSize(sf_free_list_node * head, sf_header header);
+sf_free_list_node * get_sf_free_list_node(size_t size);
 
 sf_prologue* prologue;
 sf_epilogue* epilogue;
@@ -82,44 +83,45 @@ void *sf_malloc(size_t size) {
         memcpy(ptrFreeBlock + remaining_byte -8, &free_block_footer,8);
 
         sf_free_list_node* freeBlockNode = sf_add_free_list(remaining_byte,&sf_free_list_head);
-        free_block_header.links.next = &(sf_free_list_head.head);
-        free_block_header.links.prev = &(sf_free_list_head.head);
-        sf_free_list_head.prev = freeBlockNode;
-        sf_free_list_head.next = freeBlockNode;
-        sf_show_free_lists();
+        freeBlockNode->head.links.next = (sf_header*) ptrFreeBlock;
+        freeBlockNode->head.links.prev = (sf_header*) ptrFreeBlock;
+        ((sf_header*) ptrFreeBlock)->links.next = &(freeBlockNode->head);
+        ((sf_header*) ptrFreeBlock)->links.prev = &(freeBlockNode->head);
 
         return (ptrBeginning + 48);
     }
 
     else{
-        sf_header header;
-        header.info.block_size += size;
-        if(header.info.block_size % 16 != 0){
-            header.info.block_size += (16 - (header.info.block_size % 16));
+        size_t block_size = size;
+        block_size = block_size + 8;
+        if(block_size % 16 != 0){
+            block_size += (16 - (block_size % 16));
         }
-        if(header.info.block_size < 32)
-            header.info.block_size += 16;
+        if(block_size < 32)
+            block_size += 16;
+
+
+
     }
     return NULL;
 }
 
-/*
-*sf_free_list_node function returns the sf_free_list_node that fits the size requirement, or NULL if no such block exists
-*But, I need to find the best fit block header to reduce fragmentation.
+
+
+/* This function returns an address to the sf_free_list_node which contains the size.
+**@size size that the sf_free_list_node contains.
+**@return the address to the sf_free_list_node that contains the size.
 */
-sf_free_list_node * getBlockOfFitSize(sf_free_list_node * head, sf_header header){
-        //now, head is the first node of the list
-        sf_free_list_node* current_node = (*head).next;
-        while(current_node != &sf_free_list_head){
-            if(((current_node->size) << 4) >= header.info.block_size){
-                    return current_node;
-                }
-            current_node = (*current_node).next;
-            }
-        return NULL;
+sf_free_list_node * get_sf_free_list_node(size_t size){
+    sf_free_list_node* current_node = sf_free_list_head.next;
+    while (current_node != &sf_free_list_head){
+        if(((current_node-> size) << 4) >= size){
+            return current_node;
+        }
+        current_node = (*current_node).next;
+    }
+    return NULL;
 }
-
-
 
 void sf_free(void *pp) {
     return;
