0b1833216589236e5c07393cbffcdab49511f355
Rongan Li <Rongan.li@stonybrook.edu>
2018-10-24 03:16:27 -0400

    added helper function

--- hw3/src/sfmm.c
+++ hw3/src/sfmm.c
@@ -12,9 +12,13 @@
 
 sf_free_list_node* get_sf_free_list_node(size_t size);
 sf_free_list_node* set_sf_free_list_node(size_t size);
+void removeFromFreelist(sf_header* header);
+void addFreeHeader(sf_header* header);
 
 sf_prologue* prologue;
 sf_epilogue* epilogue;
+sf_header* lastFreeHeader;
+sf_footer* lastFreeFooter;
 
 void *sf_malloc(size_t size) {
     if(size == 0)
@@ -75,6 +79,7 @@ void *sf_malloc(size_t size) {
         free_block_header.info.prev_allocated = 1;
         free_block_header.info.allocated = 0;
         memcpy(ptrFreeBlock, &free_block_header,8);
+        lastFreeHeader = (sf_header*) ptrFreeBlock;
 
         sf_footer free_block_footer;
         free_block_footer.info.requested_size = 0;
@@ -82,13 +87,14 @@ void *sf_malloc(size_t size) {
         free_block_footer.info.allocated = 0;
         free_block_footer.info.prev_allocated = 1;
         memcpy(ptrFreeBlock + remaining_byte - 8, &free_block_footer,8);
+        lastFreeFooter = (sf_footer*) (ptrFreeBlock + remaining_byte - 8);
 
         sf_free_list_node* freeBlockNode = sf_add_free_list(remaining_byte,&sf_free_list_head);
         freeBlockNode->head.links.next = (sf_header*) ptrFreeBlock;
         freeBlockNode->head.links.prev = (sf_header*) ptrFreeBlock;
         ((sf_header*) ptrFreeBlock)->links.next = &(freeBlockNode->head);
         ((sf_header*) ptrFreeBlock)->links.prev = &(freeBlockNode->head);
-
+        sf_show_heap();
         return (ptrBeginning + 48);
     }
 
@@ -101,14 +107,116 @@ void *sf_malloc(size_t size) {
         if(block_size < 32)
             block_size += 16;
         if(get_sf_free_list_node(size) == NULL){
-            void* ptrNewPage = sf_mem_grow();
-            sf_free_list_node* ptrNewFreelistNode = set_sf_free_list_node(size);
+            if (((sf_footer*)(sf_mem_end()-16)) == lastFreeFooter){
+                size_t numOfBytesHad = lastFreeFooter->info.block_size << 4;
+                while(numOfBytesHad < block_size){
+                    sf_mem_grow();
+                    epilogue = (sf_epilogue*)(sf_mem_end()  - 8);
+                    memset(epilogue,0,8);
+                    (*epilogue).footer.info.allocated = 1;
+                    numOfBytesHad += 4096;
+                }
+                //now numOfBytesHad will be the size of the new coallesce free block.
+                memset(lastFreeHeader + (lastFreeHeader->info.block_size) -8, 0, 8); //But, is it (calling memset) really necessary?
+                memcpy(sf_mem_end() - 8,epilogue,8);
+                lastFreeHeader->info.block_size = numOfBytesHad;
+                lastFreeFooter = (sf_footer*)(sf_mem_end() - 16);
+                lastFreeFooter-> info.block_size = numOfBytesHad;
+                lastFreeFooter-> info.prev_allocated = lastFreeHeader->info.prev_allocated;
+                lastFreeFooter-> info.allocated = 0;
+
+                // if(block_size == numOfBytesHad){
+                //     sf_header block_header = {size, block_size, 0,lastFreeHeader->info.prev_allocated,1};
+
+                // }
+
+            }
+            else{
+                // sf_mem_grow();
+                // sf_free_list_node* ptrNewFreelistNode = set_sf_free_list_node(size);
+            }
+
+
         }
         else{
 
         }
     }
     return NULL;
+}
+
+
+/*
+** This function removes a sf_header from the circular doubly linked list
+*@header the block header you are going to remove from the circular doubly linked list
+*/
+void removeFromFreelist(sf_header* header){
+    sf_header* prev_free_header = header->links.prev;
+    sf_header* next_free_header = header->links.next;
+    prev_free_header->links.next = next_free_header;
+    next_free_header->links.prev = next_free_header;
+}
+
+/*
+*This function coallesce the free block pointed by freeBlockHeader with previous or next block if existed, and return the pointer to the
+*coallesced free block. Otherwise, return null if previous block and next block are not free
+*/
+sf_header* coallesce(sf_header* freeBlockHeader){
+        int isLastFreeBlock;
+        if((freeBlockHeader + freeBlockHeader->info.block_size) == (sf_mem_end()-8)){
+            isLastFreeBlock = 1;
+        }
+        else{
+            isLastFreeBlock = 0;
+        }
+        if(isLastFreeBlock == 1){
+            if(freeBlockHeader->info.prev_allocated == 1){
+                return NULL;
+            }
+            else{
+                sf_footer* prevBlKfterPtr = (sf_footer*) freeBlockHeader - 8;
+                size_t prevBlkSize = prevBlKfterPtr->info.block_size;
+                size_t currBlkSize = freeBlockHeader->info.block_size;
+                sf_header* prevBlkHdPtr = (sf_header*)(prevBlKfterPtr - prevBlkSize + 8);
+                sf_footer* freeBlockFooter = (sf_footer*)freeBlockHeader + currBlkSize - 8;
+                size_t combined_blkSize = currBlkSize+ prevBlkSize;
+                prevBlkHdPtr->info.block_size = combined_blkSize;
+                freeBlockFooter->info.block_size = combined_blkSize;
+                removeFromFreelist(prevBlkHdPtr);
+                removeFromFreelist(freeBlockHeader);
+                addFreeHeader(prevBlkHdPtr);
+                memset(prevBlKfterPtr,0,8);
+                memset(freeBlockHeader,0,8);
+                return prevBlkHdPtr;
+            }
+        }
+        else{
+            sf_footer* freeBlockFooter = (sf_footer*)freeBlockHeader + currBlkSize - 8;
+
+        }
+    }
+
+
+/** This function add header of a free list to list of free lists
+**
+**/
+void addFreeHeader(sf_header* header){
+    size_t block_size = header->info.block_size;
+    if(get_sf_free_list_node(block_size) == NULL){
+        sf_free_list_node* freeNode = set_sf_free_list_node(block_size);
+        freeNode->head.links.next = header;
+        freeNode->head.links.prev = header;
+        header->links.next = &freeNode->head;
+        header->links.prev = &freeNode->head;
+    }
+    else{
+        sf_free_list_node* freeNode = get_sf_free_list_node(block_size);
+        sf_header* nextHeader = freeNode->head.links.next;
+        freeNode->head.links.next = header;
+        header->links.prev = &freeNode->head;
+        nextHeader->links.prev = header;
+        header->links.next = nextHeader;
+    }
 }
 
 /* This function add a node that contains the size specified to sf_free_list in the corrsponding spot based on its size, and return the Node being added
