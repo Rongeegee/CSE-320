148527606bd90d1e24d7ff25e0546d54e84deca2
Rongan Li <Rongan.li@stonybrook.edu>
2018-10-27 23:42:45 -0400

    add one helper function

--- hw3/src/main.c
+++ hw3/src/main.c
@@ -4,16 +4,14 @@
 int main(int argc, char const *argv[]) {
     sf_mem_init();
 
-    double* ptr = sf_malloc(3012);
+    double* ptr = sf_malloc(4016);
 
-    // *ptr = 320320320e-320;
+    *ptr = 320320320e-320;
+
     sf_show_heap();
-    sf_realloc(ptr,2500);
-
-
     printf("%e\n", *ptr);
 
-   // sf_free(ptr);
+    sf_free(ptr);
 
     sf_mem_fini();
 
